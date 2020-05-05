7b098ee1dc984358fede7ba4d968c9f6fbf42b3b
Louis FRADIN <louis.fradin@gmail.com>
2020-05-05 15:31:51 +0200

    export templates: correct installation

--- godot-updater.sh
+++ godot-updater.sh
@@ -18,7 +18,9 @@ echo "# Compile Editor for Linux"
 scons -j${THREADS} platform=x11
 
 echo "# Get version"
-VERSION=$(${GODOT_PATH}/bin/godot.x11.tools.64 --version)
+COMPLETE_VERSION=$(${GODOT_PATH}/bin/godot.x11.tools.64 --version)
+echo "Complete version is ${COMPLETE_VERSION}"
+VERSION=$(echo "${COMPLETE_VERSION}" | sed 's|.custom.*||')
 echo "Version is '${VERSION}'"
 
 echo "# Compile Export templates for Linux 64bits"
@@ -28,5 +30,5 @@ scons -j${THREADS} platform=x11 tools=no target=release_debug bits=64
 echo "# Install export templates into ${USER} home"
 TEMPLATE_DIR="${HOME}/.local/share/godot/templates/${VERSION}/"
 mkdir -p ${TEMPLATE_DIR}
-cp ${GODOT_PATH}/bin/godot.x11.opt.64 ${TEMPLATE_DIR}
-cp ${GODOT_PATH}/bin/godot.x11.opt.debug.64 ${TEMPLATE_DIR}
+cp ${GODOT_PATH}/bin/godot.x11.opt.64 ${TEMPLATE_DIR}/linux_x11_64_release
+cp ${GODOT_PATH}/bin/godot.x11.opt.debug.64 ${TEMPLATE_DIR}/linux_x11_64_debug
